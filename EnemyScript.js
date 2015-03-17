d0c35145451b905ee6c915c3ef78e4ff7dee70d8
Shugo.Ikeda <shugo.ikeda@a4works.co.jp>
2015-03-18 02:50:34 +0900

    first commit

---
+++ EnemyScript.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+function Update () {
+	transform.position.z -= 0.2;
+	transform.Rotate(1,1,1);
+	if (transform.position.z < -12.0){
+		Application.LoadLevel("GameOver");
+	}
+}
+
+ function OnCollisionEnter(){
+ 	Destroy(gameObject);
+}
+
